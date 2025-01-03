2b550133e034a572a06f8e7b8bf1f9b63d068ac5
rr967 <andre.vanhulle@gmail.com>
2025-01-03 14:53:57 +0100

    Add files via upload

--- NNfSiX/p005-ReLU-Activation.py
+++ NNfSiX/p005-ReLU-Activation.py
@@ -1,11 +1,22 @@
 import numpy as np 
 import nnfs
-from nnfs.datasets import spiral_data  # See for code: https://gist.github.com/Sentdex/454cb20ec5acf0e76ee8ab8448e6266c
+
+# from nnfs.datasets import spiral_data  # See for code: https://gist.github.com/Sentdex/454cb20ec5acf0e76ee8ab8448e6266c
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
 
 nnfs.init()
 
 X, y = spiral_data(100, 3)   
-
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
